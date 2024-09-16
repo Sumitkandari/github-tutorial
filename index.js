c67923d6191845b6716fe6325872439c631e1b61
Sumit Kandari <sumitkandari8@gmail.com>
2024-09-16 19:23:19 +0530

    new console added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let name="Sumit Kandari"
 let age="24";
 function Fun(){
-    console.log(name)
+    console.log(name);
+    console.log("new console inside bug 2349")
 }
 function aboveAge(){
     return age>=18
