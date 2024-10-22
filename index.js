56bcef06ce84731ed19ac472730df5164adb7c66
Sumit Kandari <sumitkandari8@gmail.com>
2024-10-22 21:41:08 +0530

    New changes

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-let name="Sumit Kandari"
-let age="24";
-function Fun(){
-    console.log(name)
+let grid =[[2,1,1],[1,1,0],[0,1,1]]
+let fresh=0;
+let queue=[];
+let count=0;
+for(let i=0;i<grid.length;i++){
+    for(let j=0;j<grid[0].length;j++){
+        if(grid[i][j]===1){
+            fresh++;
+        }
+        if(grid[i][j]===2){
+            queue.push([i,j]);
+        }
+    }
 }
-function aboveAge(){
-    return age>=18
+let directions=[[0,1],[-1,0],[1,0],[0,-1]];
+while(queue.length&&fresh>0){
+    [r,c]=queue.shift();
+    for(let [dr,dc] of directions){
+        row=r+dr;
+        col=c+dc;
+        if(row<0 ||col<0 ||row>=grid.length||col>=grid[0].length||grid[row][col]!=1){
+            continue;
+        }
+        else{
+            queue.push([row,col]);
+            grid[row][col]=2;
+        fresh--;
+        }
+    }
+    count++;
 }
-function addMain(){
-    console.log("Add main ")
-}
-function newMain(){
-    console.log("new Mesgfdgfdsdff")
-}
-function random(){
-
-}
+console.log(count)
